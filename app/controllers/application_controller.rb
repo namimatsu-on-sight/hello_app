12671910e934e0e5bb4525bffb623f8488bc3e1a
namimatsu.ren <namimatsuren@gmail.com>
2026-03-30 15:30:33 +0900

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  def hello
+    render html: "¡hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
+  end
+  
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+end
